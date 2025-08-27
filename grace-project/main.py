3a248af4ef13f0c46efca73382bc97f4c9ac2912
root <root@Dante.localdomain>
2025-08-27 09:59:26 +0300

    Added the collaboraive function in the knowledge graph and called the collaborative function in the main.py file using the results3 variable. I also changed the variables containing the call functions of metta files from just results to results1, results2 and results3 respectively and printed them all. The main.py fileis connected to the knowledge.metta file not the example.metta file

---
+++ grace-project/main.py
@@ -0,0 +1,52 @@
+
+try:
+    from hyperon import MeTTa
+except RuntimeError:
+    print("Failed to import MeTTa from hyperon")
+
+metta = MeTTa()
+
+
+def add_fact(text):
+    print(f"Parsing: {text}")
+    try:
+        atom = metta.parse_single(text)
+        metta.space().add_atom(atom)
+    except Exception as e:
+        print(f"Error parsing: {e}")
+
+
+def load_metta_file(knowledgeGraph):
+    try:
+        with open(knowledgeGraph, "r") as file:
+            # filter out comments and blank lines
+            facts = [line.strip() for line in file if line.strip() and not line.strip().startswith(";")]
+
+        for fact in facts:
+            print("Loading fact:", fact)
+            add_fact(fact)
+        print(f"Loaded {len(facts)} facts.")
+    except FileNotFoundError:
+        print(f"The file {knowledgeGraph} was not found")
+
+def execute_query(query: str):
+    result = metta.run(query)
+    cleaned = []
+    for r in result:
+        text = str(r)
+        # remove outer brackets and quotes, then join tokens
+        text = text.replace("(", "").replace(")", "").replace('"', "")
+        parts = text.split()
+        cleaned.append(" ".join(parts))
+    return cleaned
+    
+    #return [str(r) for r in result]
+
+# Load knowledge base first
+load_metta_file("knowledgeGraph.metta")
+
+# THEN query
+results1 = execute_query("!(recommend user1)")
+results2 = execute_query("!(recommendByDirector user1)")
+results3 = execute_query("!(recommendCollaborativeAdvanced user1)")
+print(results1, results2, results3)
